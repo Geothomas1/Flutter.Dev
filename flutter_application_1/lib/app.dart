0aff28442df7d980ac5a134e77bd615b9a477568
Geo Thomas <geothomas@cet.ac.in>
2021-06-11 18:01:00 +0530

    BuildContext over to other custom widget called basicWidget.dart

--- flutter_application_1/lib/app.dart
+++ flutter_application_1/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './widgets/basicWidget.dart';
 
 class App extends StatelessWidget {
   @override
@@ -6,22 +7,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Demo App',
       home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 320,
-            height: 320,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.only(left:100,top:150),
-            decoration: BoxDecoration(
-                color: Colors.red,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(10)),
-            child: Text('Demo App By Geo'),
-          ),
-        ),
+        body: Center(child: basicWidget()),
       ),
     );
   }
---
+++ flutter_application_1/lib/widgets/basicWidget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class basicWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width*0.4,
+      height: MediaQuery.of(context).size.height*0.8,
+      margin: EdgeInsets.all(0),
+      padding: EdgeInsets.only(left: 100, top: 150),
+      decoration: BoxDecoration(
+          color: Colors.blue,
+          border: Border.all(
+            color: Colors.black,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow:[
+            BoxShadow(
+            color:Colors.grey,
+            spreadRadius:10, 
+            blurRadius: 10,
+            offset: Offset(
+              10,10
+            ),
+          ),
+          ]),
+      child: Text('Demo App By Geo'),
+    );
+  }
+}

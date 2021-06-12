17d0f4d023db5e391fd606496d031642aa2815fb
Geo Thomas <geothomas@cet.ac.in>
2021-06-12 09:58:47 +0530

    Align Single Child

--- flutter_application_1/lib/widgets/singleChildLayouts.dart
+++ flutter_application_1/lib/widgets/singleChildLayouts.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 class SingleChildLayout extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return SizedBox(
-      width: 200,
-      height: 100,
-      child:Center(
-        child:Text("Hello World")
-      ) ,
+    return Align(
+      child: Text("Hey Geo Thomas"),
+      alignment: Alignment.center,
     );
   }
 }

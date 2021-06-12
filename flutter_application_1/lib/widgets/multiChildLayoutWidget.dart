be9f5345c76ece2e078c960e0de580e7d1ba56a5
Geo Thomas <geothomas@cet.ac.in>
2021-06-12 17:44:56 +0530

    Grid View

--- flutter_application_1/lib/widgets/multiChildLayoutWidget.dart
+++ flutter_application_1/lib/widgets/multiChildLayoutWidget.dart
@@ -4,54 +4,55 @@ class MultiChildLayoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 200,
-      color: Colors.grey,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        reverse: true,
-        padding: EdgeInsets.all(50),
-        children: <Widget>[
-          Container(
-            color: Colors.red,
-            width: 300,
-            height: 200,
-            child: Center(
-              child: Text("Aju Uncle"),
-            ),
-          ),
-          Container(
-            color: Colors.green,
-            width: 300,
-            height: 200,
-            child: Center(child: Text("Achachan")),
-          ),
-          Container(
-            color: Colors.pink,
-            width: 300,
-            height: 200,
-            child: Center(
-              child: Text("Anju"),
-            ),
-          ),
-          Container(
-            color: Colors.blue,
-            width: 300,
-            height: 200,
-            child: Center(
-              child: Text("Ammu"),
-            ),
-          ),
-          Container(
-            color: Colors.yellow,
-            width: 300,
-            height: 200,
-            child: Center(
-              child: Text("Anu"),
-            ),
-          ),
-        ],
-      ),
-    );
+        width: MediaQuery.of(context).size.width,
+        height: 200,
+        color: Colors.grey,
+        child: GridView.count(
+          crossAxisCount:3 ,
+          children: <Widget>[
+            Container(
+                color: Colors.red,
+                width: 300,
+                height: 200,
+                child: Center(
+                  child: Text("Aju Uncle"),
+                )),
+            Container(
+                color: Colors.yellow,
+                width: 300,
+                height: 200,
+                child: Center(
+                  child: Text("Achanchan"),
+                )),
+            Container(
+                color: Colors.green,
+                width: 300,
+                height: 200,
+                child: Center(
+                  child: Text("Anu"),
+                )),
+            Container(
+                color: Colors.pink,
+                width: 300,
+                height: 200,
+                child: Center(
+                  child: Text("Anju"),
+                )),
+            Container(
+                color: Colors.deepPurpleAccent,
+                width: 300,
+                height: 200,
+                child: Center(
+                  child: Text("Arun Kumar"),
+                )),
+            Container(
+                color: Colors.orange,
+                width: 300,
+                height: 200,
+                child: Center(
+                  child: Text("Aparan"),
+                )),
+          ],
+        ));
   }
 }

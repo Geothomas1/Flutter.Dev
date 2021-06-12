877d5df48b4efe7028861c50d398f7457beac286
Geo Thomas <geothomas@cet.ac.in>
2021-06-12 17:28:00 +0530

    ListView

--- flutter_application_1/lib/widgets/multiChildLayoutWidget.dart
+++ flutter_application_1/lib/widgets/multiChildLayoutWidget.dart
@@ -5,43 +5,49 @@ class MultiChildLayoutWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
+      height: 200,
       color: Colors.grey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        verticalDirection: VerticalDirection.down,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        reverse: true,
+        padding: EdgeInsets.all(50),
         children: <Widget>[
           Container(
-            width: 200,
-            height: 200,
-            color: Colors.green,
-            child: Center(
-              child: Text("One"),
-            ),
-          ),
-          Container(
-            width: 200,
-            height: 200,
-            color: Colors.blue,
-            child: Center(
-              child: Text("Two"),
-            ),
-          ),
-          Container(
-            width: 200,
-            height: 200,
-            color: Colors.yellow,
-            child: Center(
-              child: Text("Three"),
-            ),
-          ),
-          Container(
-            width: 200,
-            height: 200,
             color: Colors.red,
+            width: 300,
+            height: 200,
             child: Center(
-              child: Text("Four"),
+              child: Text("Aju Uncle"),
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            width: 300,
+            height: 200,
+            child: Center(child: Text("Achachan")),
+          ),
+          Container(
+            color: Colors.pink,
+            width: 300,
+            height: 200,
+            child: Center(
+              child: Text("Anju"),
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            width: 300,
+            height: 200,
+            child: Center(
+              child: Text("Ammu"),
+            ),
+          ),
+          Container(
+            color: Colors.yellow,
+            width: 300,
+            height: 200,
+            child: Center(
+              child: Text("Anu"),
             ),
           ),
         ],

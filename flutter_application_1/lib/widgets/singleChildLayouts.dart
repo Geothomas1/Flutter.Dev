38d4b3414e4f32e7704c093ac50d66eeb0421908
Geo Thomas <geothomas@cet.ac.in>
2021-06-12 09:44:03 +0530

    Center single child widget

--- flutter_application_1/lib/widgets/singleChildLayouts.dart
+++ flutter_application_1/lib/widgets/singleChildLayouts.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 class SingleChildLayout extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Padding(
-      padding:EdgeInsets.all(100),
-      child: Text("Hello World"),
-      );
+    return Container(
+      width: 400,
+      height: 400,
+      color: Colors.green,
+      child:Center(
+      child: Text("HELLO WORLD"),
+    )
+    );
   }
 }

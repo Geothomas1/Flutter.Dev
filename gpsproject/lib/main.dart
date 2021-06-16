fa860d4e00038a4061d101d73a23c4756f7a5835
Geo Thomas <geothomas@cet.ac.in>
2021-06-16 18:55:13 +0530

    gps project

---
+++ gpsproject/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import './Screens/login_screen.dart';
+import './Screens/registration_screen.dart';
+import './Screens/welcome_screen.dart';
+
+
+
+void main() => runApp(Sukhadyathra());
+
+class Sukhadyathra extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id ,
+      routes: {
+        WelcomeScreen.id:(context)=>WelcomeScreen(),
+        LoginScreen.id:(context)=>LoginScreen(),
+        RegistrationScreen.id:(context)=>RegistrationScreen(),
+
+      },
+    );
+  }
+}

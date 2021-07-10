96f9b316681447bb133b7fe82159db732c4d3f46
Geo Thomas <geothomas@cet.ac.in>
2021-07-10 21:40:13 +0530

    scaffold leading,action,floating button and drawer action

--- flutter_application_1/lib/app.dart
+++ flutter_application_1/lib/app.dart
@@ -8,6 +8,35 @@ class App extends StatelessWidget {
       title: 'Demo App',
       home: Scaffold(
         body: ButtonWidget(),
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen,
+          elevation: 10,
+          title: Text("Demand Me"),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () => {print("Action button ")},
+              icon: Icon(Icons.bookmark),
+            ),
+            IconButton(
+              onPressed: () => {print("Action 2 button")},
+              icon: Icon(Icons.mail),
+            ),
+            IconButton(
+              onPressed: () => {print("Action 3 button")},
+              icon: Icon(Icons.message),
+            )
+          ],
+        ),
+        floatingActionButton: IconButton(
+          onPressed: ()=>{
+            print("Click on floting action button")
+          },
+          icon: Icon(Icons.publish),
+          color: Colors.red,
+        ),
+        drawer: Drawer(),
       ),
     );
   }

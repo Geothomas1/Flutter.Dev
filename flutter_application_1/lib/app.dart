fc14a2ef5dbbf33d84abee883a7cc826e97868df
Geo Thomas <geothomas@cet.ac.in>
2021-07-11 10:18:16 +0530

    Listtile sidemenu

--- flutter_application_1/lib/app.dart
+++ flutter_application_1/lib/app.dart
@@ -30,13 +30,55 @@ class App extends StatelessWidget {
           ],
         ),
         floatingActionButton: IconButton(
-          onPressed: ()=>{
-            print("Click on floting action button")
-          },
+          onPressed: () => {print("Click on floting action button")},
           icon: Icon(Icons.publish),
           color: Colors.red,
         ),
-        drawer: Drawer(),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Text("Demand me menu"),
+                decoration: BoxDecoration(color: Colors.green),
+              ),
+              ListTile(
+                title: Text("Post Demand"),
+                subtitle: Text("post the demand you need"),
+                onTap: () => {print("Post Demand pressed")},
+                leading: Icon(Icons.favorite),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title: Text("Posted Jobs"),
+                subtitle: Text("view posted Jobs"),
+                onTap: () => {print("view Demand pressed")},
+                leading: Icon(Icons.post_add),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title: Text("Profile"),
+                subtitle: Text("View profile"),
+                onTap: () => {print("Profile pressed")},
+                leading: Icon(Icons.verified_user),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title: Text("Setting"),
+                subtitle: Text("Edit your settings"),
+                onTap: () => {print("Post Demand pressed")},
+                leading: Icon(Icons.settings),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title: Text("Signout"),
+                subtitle: Text("Signout from Demand me"),
+                onTap: () => {print("Signout Demand pressed")},
+                leading: Icon(Icons.logout),
+                trailing: Icon(Icons.arrow_forward),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
